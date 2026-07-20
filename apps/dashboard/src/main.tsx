a563ad6b7a30388b797525e873abb7a3d9a7a07f
Arthur <arthurjenckdev@gmail.com>
2026-07-20 17:00:00 +0200

    feat/analytics: added vercel analytics package

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,8 +1,9 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { Analytics } from '@vercel/analytics/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createRouter, RouterProvider } from '@tanstack/react-router';
-import { routeTree } from './routeTree.gen';
 import './index.css';
+import { routeTree } from './routeTree.gen';
 
 const router = createRouter({ routeTree, defaultPreload: 'intent' });
 
@@ -15,5 +16,6 @@ declare module '@tanstack/react-router' {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+    <Analytics />
+  </StrictMode>
 );
